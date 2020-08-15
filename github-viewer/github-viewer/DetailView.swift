822f50a4da242619b7f078d4666953dba8f59381
Mike Kluev <mike.kluev@guess-what.com>
2020-08-15 02:35:40 +0100

    introducing detailView

---
+++ github-viewer/github-viewer/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  github-viewer
+//
+//  Created by Mike on 15/08/2020.
+//  Copyright © 2020 Mike Kluev. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
